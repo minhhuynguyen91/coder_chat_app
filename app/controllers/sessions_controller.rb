37a396a1bf73e5d3fbff637a9eeefbaba1f549b5
Minh Nguyen <ng.huyminh91@gmail.com>
2017-02-21 07:19:49 +0700

    Add signup signin signout
    Signed-off-by: Minh Nguyen <ng.huyminh91@gmail.com>

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    if @user = User.find_by_username(params[:username]) and @user.authenticate(params[:password])
+
+      session[:user_id] = @user.id
+      flash[:success] = "Sign in successfully"
+      redirect_to root_path
+    else
+      flash[:error] = "Invalid username or password"
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "Singout successfully"
+    redirect_to root_path
+  end
+
+end
